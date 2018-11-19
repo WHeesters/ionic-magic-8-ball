33c5f1b7cadd9ddd394dd58ece2b27a8d99e256f
Wesley <wesley.heesters@easyflex.nl>
2018-11-19 09:01:19 +0100

    Adds service logic and tests

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MagicBallService} from '../magic-ball.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    answer: any = '...';
+
+    constructor(public magicBall: MagicBallService) {
+
+    }
+
+    showAnswer() {
+        this.answer = this.magicBall.getRandomAnswer();
+    }
 
 }
--- src/app/magic-ball.service.spec.ts
+++ src/app/magic-ball.service.spec.ts
@@ -1,12 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { MagicBallService } from './magic-ball.service';
+import {MagicBallService} from './magic-ball.service';
+
+let magicBall = null;
 
 describe('MagicBallService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    beforeEach(() => TestBed.configureTestingModule({}));
+    beforeEach(() => {
+        magicBall = new MagicBallService();
+    });
 
-  it('should be created', () => {
-    const service: MagicBallService = TestBed.get(MagicBallService);
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        const service: MagicBallService = TestBed.get(MagicBallService);
+        expect(service).toBeTruthy();
+    });
+    it('should return a non empty array', () => {
+        let result = magicBall.getAnswers();
+
+        expect(Array.isArray(result)).toBeTruthy();
+        expect(result.length).toBeGreaterThan(0);
+    });
+    it('should return one random answer as a string', () => {
+        expect(typeof magicBall.getRandomAnswer()).toBe('string');
+    });
+    it('should have both yes and no available in result set', () => {
+
+        let result = magicBall.getAnswers();
+
+        expect(result).toContain('Yes');
+        expect(result).toContain('No');
+
+    });
 });
